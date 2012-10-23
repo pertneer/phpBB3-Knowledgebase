36cc40bc435e52fc786b62129f452ba5e114ea3a
pertneer <pertneer@gmail.com>
2012-10-23 18:47:08 -0500

    ability to see mod reports in any location

--- root/adm/mods/kb_version.php
+++ root/adm/mods/kb_version.php
@@ -1,30 +1,37 @@
-<?php
+<?php
+
 /**
-** @author Pertneer
+*
+* @author Pertneer
 * @package phpbb3-knowledgebase
 * @version $Id adm/mods/kb_version.php
 * @copyright (c) 2012 pertneer
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
-*/
+*/
+
 /**
 * @ignore
-*/
+*/
+
 if (!defined('IN_PHPBB'))
 {
 	exit;
-}
+}
+
 class kb_version
-{
+{
+
 	function version()
 	{
 		return array(
 			'author'	=> 'Pertneer',
 			'title'		=> 'phpBB3-Knowledgebase',
 			'tag'		=> 'kb',
-			'version'	=> '1.0.2',
+			'version'	=> '1.0.3',
 			'file'		=> array('pertneer.net', 'mods', 'knowledgebase.xml'),
 		);
 	}
-}
+}
+
 ?>
