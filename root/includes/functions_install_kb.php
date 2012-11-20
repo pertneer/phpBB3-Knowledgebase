pertneer/phpBB3-Knowledgebase

commit 212fbaf54ec3f8587866be6155a7c2c3e3a17558

Add ability to reset plugins to installed state

root/includes/functions_install_kb.php
@@ -113,15 +113,7 @@ function kb_update_versions()
 			'config_add'	=> array(
 				array('kb_default_rating', 3),
 			),
-		),
-		
-		'1.0.4a' => array(
-		
-		),
-		
-		'1.0.4b' => array(
-		
-		),
+		)
 	);
 	
 	return $versions;

root/adm/style/acp_kb_health.html
@@ -51,6 +51,12 @@
 					<input class="button1" type="submit" id="reset_perms" name="reset_perms" value="{L_RUN_NOW}" />						
 				</dd>
 			</dl>
+			<dl>
+				<dt><label for="reset_plugins">{L_RESET_PLUGINS}:</label><br /><span>{L_RESET_PLUGINS_CONFIRM}</span></dt>
+				<dd>					
+					<input class="button1" type="submit" id="reset_plugins" name="reset_plugins" value="{L_RUN_NOW}" />						
+				</dd>
+			</dl>
 		</fieldset>
 	{S_FORM_TOKEN}
 	</form>

root/includes/acp/acp_kb.php
@@ -35,6 +35,7 @@ class acp_kb
 		include($phpbb_root_path . 'includes/constants_kb.' . $phpEx);
 		include($phpbb_root_path . 'includes/functions_kb.' . $phpEx);
 		include($phpbb_root_path . 'includes/functions_plugins_kb.' . $phpEx);
+		include($phpbb_root_path . 'includes/functions_install_kb.' . $phpEx);
 
 		$action	= request_var('action', '');
 		$submit = (isset($_POST['submit'])) ? true : false;		
@@ -426,6 +427,28 @@ class acp_kb
 						confirm_box(false, 'RESET_PERMS', $hidden_fields);
 					}
 				}
+				
+				$reset_plugins = (isset($_POST['reset_plugins'])) ? true : false;	
+				if ($reset_plugins)
+				{
+					if(confirm_box(true))
+					{
+						$sql = 'TRUNCATE TABLE ' . KB_PLUGIN_TABLE;
+						$db->sql_query($sql);
+						
+						kb_install_perm_plugins('reset', 'install');
+						add_log('admin', 'LOG_KB_RESET_PLUGINS');
+						
+						trigger_error($user->lang['KB_RESET_PLUGINS'] . adm_back_link($this->u_action));
+					}
+					else
+					{
+						$hidden_fields = build_hidden_fields(array(
+							'reset_plugins'	=> true,
+						));
+						confirm_box(false, 'RESET_PLUGINS', $hidden_fields);
+					}
+				}
 			break;
 
 			default:
@@ -508,7 +531,7 @@ class acp_kb
 
 				'S_ERROR'			=> (sizeof($error)) ? true : false,
 				'ERROR_MSG'			=> implode('<br />', $error),
-				'KB_GIT'			=> true,
+				//'KB_GIT'			=> true,
 
 				'U_ACTION'			=> $this->u_action)
 			);

root/language/en/mods/info_acp_kb.php
@@ -89,6 +89,7 @@ $lang = array_merge($lang, array(
 	'KB_PERCENT'							=> 'Percent',
 	'KB_PIXELS'								=> 'Pixels',
 	'KB_RESET_DB'							=> 'The database has been successfully reset.',
+	'KB_RESET_PLUGINS'						=> 'Plugins have been successfully reset',
 	'KB_RESET_PERMS'						=> 'The permissions have successfully been reset.',
 	'KB_RIGHT_MENU_TYPE'					=> 'Width type',
 	'KB_RIGHT_MENU_WIDTH'					=> 'Right menu width',
@@ -126,11 +127,13 @@ $lang = array_merge($lang, array(
 	'LOG_U_KB_ROLE_REMOVED'					=> '<strong>KB role removed</strong><br /> - %1$s',
 
 	'RESET_DB'								=> 'Reset Database',
-	'RESET_DB_CONFIRM'						=> 'This will reset the Knowledge Base database, not removing the tables, but emptying them so you have a fresh install ready to go. It will also remove all attachments associated with the mod. Warning: This cannot be undone.',
+	'RESET_DB_CONFIRM'						=> '<span style="color:#ff0000"><b>Warning: This cannot be undone</b></span>. <br />This will reset the Knowledge Base database, not removing the tables, but emptying them so you have a fresh install ready to go. It will also remove all attachments associated with the mod.',
 	'RESET_PERMS'							=> 'Reset Permissions',
-	'RESET_PERMS_CONFIRM'					=> 'This will reset all Knowledge Base permissions, so that you will have to remake them all from the start. It will also remove Knowledge Base related roles. Warning: This cannot be undone.',
+	'RESET_PERMS_CONFIRM'					=> '<span style="color:#ff0000"><b>Warning: This cannot be undone</b></span>. <br />This will reset all Knowledge Base permissions, so that you will have to remake them all from the start. It will also remove Knowledge Base related roles.',
+	'RESET_PLUGINS'							=> 'Reset Plugins',
+	'RESET_PLUGINS_CONFIRM'					=> '<span style="color:#ff0000"><b>Warning: This cannot be undone</b></span>. <br />This will reset all Knowledge Base plugins back to their original installion settings.',
 
-	'UNINSTALL_CONFIRM'						=> 'This will uninstall the mod completely, removing both data, permissions, configs and tables. Warning: This cannot be undone.',
+	'UNINSTALL_CONFIRM'						=> '<span style="color:#ff0000"><b>Warning: This cannot be undone</b></span>. <br />This will uninstall the mod completely, removing both data, permissions, configs and tables.',
 ));
 
 ?>

commit e5e7836942518b23b75dd7d1a7486c70d8daa3a3

Changed error caused by commit 212fbaf54ec3f8587866be6155a7c2c3e3a17558

root/includes/functions_install_kb.php
@@ -114,6 +114,12 @@ function kb_update_versions()
 				array('kb_default_rating', 3),
 			),
 		)
+		'1.0.4a' => array(
+			// Nothing here
+		),
+		'1.0.4b' => array(
+			// Nothing here
+		),
 	);
 	
 	return $versions;